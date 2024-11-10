ea5dc683010386aa8c563dbc70e716a5d555b5f1
Rodolfo <rjmoraza@gmail.com>
2024-11-10 17:40:53 -0600

    Hero model and basic animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     Vector3 movementVector;
+    Vector3 animVector;
     float yawValue;
     float pitchValue;
-    bool isAiming = false;
+    float aim;
+    float aimTarget;
+    bool canWalk = true;
+    float waterCooldown = 0;
     
-
     [SerializeField]
     Animator anim;
+
+    [SerializeField]
+    Rig rig;
 
     [SerializeField]
     GameObject targetPoint;
@@ -22,30 +29,64 @@ public class PlayerController : MonoBehaviour
     float movementSpeed = 3;
 
     [SerializeField]
+    float maxAimHeight = 2;
+
+    [SerializeField]
+    float minAimHeight = -1;
+
+    [SerializeField]
     float rotationSpeed = 60;
 
     [SerializeField]
-    float maxCameraAngle = 45;
+    float maxWaterCooldown = 10;
+
+    [SerializeField]
+    GameObject shotgun;
+
+    [SerializeField]
+    GameObject shotgunFirepoint;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        shotgun.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(isAiming);
+        if (waterCooldown > 0) waterCooldown -= Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
-        Vector3 heading = (transform.forward * movementVector.y + transform.right * movementVector.x);
-        rb.velocity = heading * movementSpeed + Vector3.up * rb.velocity.y;
-        Quaternion deltaRotation = Quaternion.Euler(0, yawValue * rotationSpeed * Time.fixedDeltaTime, 0);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-        targetPoint.transform.localPosition = new Vector3(0, pitchValue, 5);
+        if(canWalk)
+        {
+            Vector3 heading = (transform.forward * movementVector.y + transform.right * movementVector.x);
+            rb.velocity = heading * movementSpeed + Vector3.up * rb.velocity.y;
+            Quaternion deltaRotation = Quaternion.Euler(0, yawValue * rotationSpeed * Time.fixedDeltaTime, 0);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+            targetPoint.transform.localPosition = new Vector3(0, pitchValue, 5);
+
+            if (aim < aimTarget - 0.05f) aim += Time.fixedDeltaTime * 3;
+            else if (aim > aimTarget + 0.05f) aim -= Time.fixedDeltaTime * 3;
+            if (aim < 0.5f && shotgun.activeSelf) shotgun.SetActive(false);
+            if (aim > 0.5f && !shotgun.activeSelf) shotgun.SetActive(true);
+            rig.weight = aim;
+
+            Vector3 actualVelocity = new Vector3(movementVector.x, 0, movementVector.y).normalized;
+            animVector = Vector3.Lerp(animVector, actualVelocity, Time.fixedDeltaTime * 5);
+            anim.SetFloat("ZVelocity", animVector.z);
+            anim.SetFloat("XVelocity", animVector.x);
+        }
+        else
+        {
+            if (aim > 0) aim -= Time.fixedDeltaTime * 5;
+            rig.weight = aim;
+            anim.SetFloat("XVelocity", 0);
+            anim.SetFloat("ZVelocity", 0);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext c)
@@ -59,14 +100,16 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 lookValue = c.ReadValue<Vector2>();
         yawValue = lookValue.x;
-        pitchValue = Mathf.Clamp(pitchValue + lookValue.y, -1, 1);
+        pitchValue = Mathf.Clamp(pitchValue + lookValue.y * Time.fixedDeltaTime, minAimHeight, maxAimHeight);
     }
 
     public void OnLook(InputAction.CallbackContext c)
     {
         Vector2 lookValue = c.ReadValue<Vector2>();
         yawValue = lookValue.x;
-        pitchValue = lookValue.y;       
+
+        if (lookValue.y > 0) pitchValue = lookValue.y * maxAimHeight;
+        else if (lookValue.y < 0) pitchValue = lookValue.y * -minAimHeight;        
     }
 
     public void OnFire1(InputAction.CallbackContext c)
@@ -76,20 +119,32 @@ public class PlayerController : MonoBehaviour
 
     public void OnFire2(InputAction.CallbackContext c)
     {
-
+        if(waterCooldown <= 0)
+        {
+            waterCooldown = maxWaterCooldown;
+            StartCoroutine(ThrowWater());
+        }
     }
 
     public void OnAim(InputAction.CallbackContext c)
     {
         if (c.performed)
         {
-            isAiming = true;
+            aimTarget = 1;
             CameraController.instance.EngageAim();
         }
         else if (c.canceled)
         {
-            isAiming = false;
+            aimTarget = 0;
             CameraController.instance.DisengageAim();
         }
+    }
+
+    IEnumerator ThrowWater()
+    {
+        canWalk = false;
+        anim.SetTrigger("Throw");
+        yield return new WaitForSeconds(2.1f);
+        canWalk = true;
     }
 }
